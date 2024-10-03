aebcad45e5033b463240c0dcfa1f768886d633dc
nidr0x <nidr0x@gmail.com>
2024-10-03 20:28:11 +0200

    refactor(talos): move secrets to a separate file

--- talos/backend.tf
+++ talos/backend.tf
@@ -1,21 +1,15 @@
-{
-	"data": "ENC[AES256_GCM,data:YApTSvhGwmBvQaFB8xK2WjS5afcpY+/mAwK5fcYmfnRBb2l29pYDPeWA/AZ6exHgh3ZnWjSCOc6u/9VpDjvF4dMTikwkAu9Ra+OJYjI73pP2fc9uoy+TFPbF/w4GEPcFuVvrCx9XzxSWxWj3e2FYGd9b8lwF1DtCHtyPPSBbTYIBHC0lPBhwYTxMKCVLNyFDHnWCWVNf0nKwcOFona567VtWqHBACOilSIoo3ookue8DEqePQSuaqluWJG0zEnFIuwYT38hPgQtVkRIFaXFEiqdmoeytnKthgKNIDOr/Vkqi2WJqUWcuXpQZZpfusSeYNM3cPS301wKZfX5fZGBndmyYShGTFzV2qpueMQAFLhZXu/37X3K2eTkloxhA+CvexKJJ6Nmra8T0s4MRDn8Afap1zBIMnQgIGe+W4frL6kxGuy1A59DOOOslWbr/uBDaS7F0BqDXahoQimTD+VVxzujTailAWXg8zryZnzRrWyrRmGag8kD+mrr1QIJtQDj7zx73E3akEwHtq1DVO1qReWr5kFdJJJ7XEnp2PfpM4oN9b6FxyHphrV9+2CqDnoChZnj4t8tsCsPC4i6cK/7XluLMKX+jKPqkizWrH3b/xiKeHMrK/xPzT2BskZfudIlt64eIB5CqHTd21lqVcI24Pmm1fDF8AtF/RdwllXx9t0zz98FC8/jbjU3QXbalnP9sxF2DszTvlfvYQIZvM1Zc7N7TBXoe7ztHoOdAMDHB7kOHnTzU4Gpyi5XfhlxQBw6FLlpeErZRM/dAtCKAzAX07uxAe4xfJPAJnFBwP1ymzK2h66/SPmpP0hlsLbjqEw5M3AWgv1wiCH/TXInQxyREzZ5b/br9VExAROOGjehp1AewbKby0bdEIWksIaRY8ihxl6J9Z3xYumR8pS7EgM+RwCx92R2h7+Xr5Snv0iHe,iv:7KZ95Sg6Kkg5KMYIY+r2ID7xvOUml4+BAVFRimhcw8s=,tag:T1ZzdEIzVyrsvd0tjavxBg==,type:str]",
-	"sops": {
-		"kms": null,
-		"gcp_kms": null,
-		"azure_kv": null,
-		"hc_vault": null,
-		"age": null,
-		"lastmodified": "2024-10-03T07:21:09Z",
-		"mac": "ENC[AES256_GCM,data:GVj/XeZH/OimjcfNUSalPFFr+EGP0Kk7j7yqupiljEdvqSMsZxyVz0iEMJigktwpvnb7PcZuu97vihWprKpOq8gaX1FCnRjlK3LYQuIiv8qQP+1bB37YXseddEcMe/0+f6KelPW9VnQKnY5lYrnAQq06FOP/Mo3i67zixYj2qhI=,iv:ZwCBBc/BiVGTE37c/dK1XkPDgHfy0n7iNV8DDQUQiYk=,tag:fGXvnU4ObIWvNla/VP+ALA==,type:str]",
-		"pgp": [
-			{
-				"created_at": "2024-10-03T07:21:09Z",
-				"enc": "-----BEGIN PGP MESSAGE-----\n\nhF4DJbjGsAZN+psSAQdAKmTOf/cy7eJC+xnwQAiWIntUPA1JlefekMZ4DTKqamsw\n6YCoh/20NT7OnpWloYrtPToftWWvd/50CcbeI5cMjF/KqESVCbnQOpx4ta+66gEX\n1GYBCQIQgIUxfFrOojFJ2hlgr9yRg9RZZk+l737oFQoPHGNSdutqjLf4fpwnvreO\nwZOSxxhOPGQ/pVHCUniXfL26sMT4fgvvT/GZgTw/VnwaWOuHQB2e/dDVOBJy3f1P\nJ5OZj2ha8Pw=\n=wKDY\n-----END PGP MESSAGE-----",
-				"fp": "12748A8CC8A832F0FC55A9B09A2D450982F5BFDC"
-			}
-		],
-		"unencrypted_suffix": "_unencrypted",
-		"version": "3.9.0"
-	}
+terraform {
+  backend "s3" {
+    bucket                      = "nidr0x-homelab"
+    key                         = "talos/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+
+    encrypt = true
+  }
 }
